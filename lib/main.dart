8ac39ca73bd305c8022d68e76cba09a43d9ab489
vinnatz <bezelx1@gmail.com>
2023-09-19 13:09:58 +0700

    Order cart change

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:product_listtt/home.dart';
+=======
+import 'package:flutter_application/pages/HomePage.dart';
+
+import 'pages/CartScreen.dart';
+>>>>>>> ae68534 (Order cart change)
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: 'Poppins',
       ),
       home: const Home(),
+=======
+      title: "IEatBread",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xffFFFFFF),
+      ),
+      routes: {
+        // "/": (context) => HomePage(),
+        "/": (context) => CartScreen(),
+      },
+>>>>>>> ae68534 (Order cart change)
     );
   }
 }
