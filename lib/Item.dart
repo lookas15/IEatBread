9c460f8981253f46c3654cac016324e4128dc9fa
ThePlatypus-Person <Ornithorhynchus0920@protonmail.com>
2023-09-20 14:50:46 +0700

    Added description, remove Sphere.dart

--- lib/Item.dart
+++ lib/Item.dart
@@ -2,6 +2,7 @@ class Item {
     String? name;
     String? path;
     int price = 0;
+    String? desc;
 
-    Item(String this.name, String this.path, int this.price);
+    Item(String this.name, String this.path, int this.price, String this.desc);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,21 @@ class ItemList extends StatefulWidget {
 
 class _ItemListState extends State<ItemList> {
     List<Item> items = [
-        Item("Anpan", 'images/anpan.jpg', 10000),
-        Item("Bagel", 'images/bagel.jpeg', 11000),
-        Item("Baguette", 'images/baguette.jpg', 12000),
-        Item("Banana Bread", 'images/bananaBread.jpeg', 13000),
-        Item("Boule", 'images/boule.jpeg', 14000),
-        Item("Breadstick", 'images/breadstick.jpg', 15000),
-        Item("Brioche", 'images/brioche.jpeg', 16000),
-        Item("Challah", 'images/challah.jpg', 17000),
-        Item("Croissant", 'images/croissant.jpeg', 18000),
-        Item("Faluche", 'images/faluche.jpeg', 19000),
-        Item("Ficelle", 'images/ficelle.jpg', 20000),
-        Item("Fougasse", 'images/fougasse.jpeg', 21000),
-        Item("Pain Brie", 'images/painBrie.jpeg', 22000),
-        Item("Pain de Campagne", 'images/painDeCampagne.jpeg', 23000),
-        Item("Pain de Mie", 'images/painDeMie.jpeg', 24000)
+        Item("Anpan", 'images/anpan.jpg', 10000, 'A Japanese sweet roll most commonly filled with red bean paste.'),
+        Item("Bagel", 'images/bagel.jpeg', 11000, 'A bread roll originating in the Jewish communities of Poland.'),
+        Item("Baguette", 'images/baguette.jpg', 12000, 'A long, thin type of bread of French origin that is commonly made from basic lean dough.'),
+        Item("Banana Bread", 'images/bananaBread.jpeg', 13000, 'A type of sweet bread made from mashed bananas. It is often a moist and sweet quick bread.'),
+        Item("Boule", 'images/boule.jpeg', 14000, 'A traditional shape of French bread resembling a squashed ball.'),
+        Item("Breadstick", 'images/breadstick.jpg', 15000, 'Pencil-sized sticks of crisp, dry baked bread that originated in Piedmont, a region of Italy.'),
+        Item("Brioche", 'images/brioche.jpeg', 16000, 'A bread of French origin whose high egg and butter content gives it a rich and tender crumb.'),
+        Item("Challah", 'images/challah.jpg', 17000, 'A special bread of Ashkenazi Jewish origin, usually braided and typically eaten on ceremonial occasions.'),
+        Item("Croissant", 'images/croissant.jpeg', 18000, 'A buttery, flaky, viennoiserie pastry inspired by the shape of the Austrian kipferl'),
+        Item("Faluche", 'images/faluche.jpeg', 19000, 'A traditional bread in the Nord-Pas-de-Calais region of northern France and the Tournai region of southern Belgium.'),
+        Item("Ficelle", 'images/ficelle.jpg', 20000, 'A type of French bread loaf, made with yeast and similar to a baguette but much thinner.'),
+        Item("Fougasse", 'images/fougasse.jpeg', 21000, 'A type of bread typically associated with Provence but found (with variations) in other regions.'),
+        Item("Pain brié", 'images/painBrie.jpeg', 22000, 'A traditional Normandy bread. Its name comes from the pounding of the dough "brie", meaning "to pound".'),
+        Item("Pain de Campagne", 'images/painDeCampagne.jpeg', 23000, 'A large round loaf ("miche") made from either natural leavening or baker\'s yeast.'),
+        Item("Pain de Mie", 'images/painDeMie.jpeg', 24000, 'A white or brown bread with a thin, soft crust. It is mostly sold sliced and packaged in France.')
     ];
 
     @override
@@ -83,7 +83,14 @@ class _ItemListState extends State<ItemList> {
                                                     Text('Rp ${item.price}'),
                                                     Expanded(
                                                         flex: 1,
-                                                        child: Text('Put description here', textAlign: TextAlign.center),
+                                                        child: ClipRRect(
+                                                        borderRadius: BorderRadius.circular(20), // Image Border
+                                                            child: Container(
+                                                                padding: EdgeInsets.all(10),
+                                                                color: Colors.grey[50],
+                                                                child: Text('${item.desc}', textAlign: TextAlign.left),
+                                                            ), // Container
+                                                        ), // ClipRRect
                                                     ), // Expanded
                                                 ], // Column
                                             ),
