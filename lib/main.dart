569b743bf34ec68127042fd25c701abf4623f837
vinnatz <bezelx1@gmail.com>
2023-09-22 22:59:51 +0700

    Edit tittle

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IEatBread',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Poppins',
