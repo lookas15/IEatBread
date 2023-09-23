6defd57e2088b8ecedb23672586fe528dce57115
vinnatz <bezelx1@gmail.com>
2023-09-23 11:20:33 +0700

    Adding feature for filtering category

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:product_listtt/menupage.dart';
-import 'package:product_listtt/widget/navbar_widget.dart';
+import 'package:product_listtt/Widgets/navbar_widget.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,13 +9,17 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController? _tabController;
+
+  final List<String> categories = ['Deals', 'Bread', 'Pastry', 'Drinks'];
+  int selectedTabIndex = 0; // Tab awal yang dipilih
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(vsync: this, length: 4);
+    _tabController =
+        TabController(vsync: this, length: categories.length, initialIndex: 0);
   }
 
   @override
@@ -23,12 +27,14 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 240, 240, 240),
       appBar: AppBar(
-        title: Text('iEatBread',
+        title: Text(
+          'iEatBread',
           style: TextStyle(
             fontSize: 20.0,
             color: Colors.white,
             fontWeight: FontWeight.bold,
-          )),
+          ),
+        ),
         backgroundColor: Color.fromARGB(255, 245, 89, 81),
         elevation: 0.0,
         centerTitle: true,
@@ -49,78 +55,45 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
             ),
             onPressed: () {},
           ),
-        ]
-      ),
-      body: ListView(
-        padding: const EdgeInsets.only(left: 18.0),
-        children: [
-          // const Text ('Menu',
-          //   style: TextStyle(
-          //     fontSize: 42.0,
-          //     fontWeight: FontWeight.bold,
-          //   ),
-          // ),
-          TabBar(
-            controller: _tabController,
-            indicatorColor: Colors.transparent,
-            labelColor: const Color.fromARGB(255, 245, 89, 81),
-            isScrollable: true,
-            // labelPadding: const EdgeInsets.symmetric(horizontal: 50),
-            unselectedLabelColor: Color.fromARGB(255, 137, 137, 137),
-            padding: EdgeInsets.fromLTRB(25,5,5,5),
-            tabs: const [
-              Tab(
-                child: Text('Deals',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              Tab(
-                child: Text('Bread',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                )
-              ),
-              Tab(
-                child: Text('Pastry',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              Tab(
-                child: Text('Drinks',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height,
-            width: double.infinity,
-            child: TabBarView(
-              controller: _tabController,
-              children: const [
-                MenuPage(),
-                MenuPage(),
-                MenuPage(),
-                MenuPage(),
-              ],
-            ),
-          ),
-          
         ],
       ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   backgroundColor: Color.fromARGB(255, 245, 89, 81),
-      //   child: const Icon(Icons.fastfood),
-      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: const Color.fromARGB(255, 245, 89, 81),
+              isScrollable: true,
+              unselectedLabelColor: Color.fromARGB(255, 137, 137, 137),
+              padding: EdgeInsets.fromLTRB(40, 5, 5, 5),
+              tabs: categories.map((category) {
+                return Tab(
+                  child: Text(
+                    category,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                );
+              }).toList(),
+              onTap: (index) {
+                setState(() {
+                  selectedTabIndex = index;
+                });
+              },
+            ),
+          ),
+          SliverFillRemaining(
+            child: TabBarView(
+              controller: _tabController,
+              children: categories.map((category) {
+                return MenuPage(category: category);
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: const NavbarWidget(),
     );
   }
--- lib/menupage.dart
+++ lib/menupage.dart
@@ -2,38 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:product_listtt/data/menu.dart';
 
 class MenuPage extends StatelessWidget {
-  const MenuPage({Key? key}) : super(key: key);
+  // const MenuPage({Key? key}) : super(key: key);
+  final String category;
+  const MenuPage({Key? key, required this.category}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 240, 240, 240),
-        body: ListView(
-          children: [
-            // const SizedBox(height: 10.0),
-            Container(
-                padding: const EdgeInsets.only(right: 15.0),
-                width: MediaQuery.of(context).size.width - 30.0,
-                height: MediaQuery.of(context).size.height - 180.0,
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 15.0,
-                    childAspectRatio: 0.63,
-                  ),
-                  itemBuilder: (context, index) {
-                    return _buildCard(listMenu[index], context);
-                  },
-                  itemCount: listMenu.length,
-                )),
-          ],
-        ));
+      backgroundColor: Color.fromARGB(255, 240, 240, 240),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(15,0,15,0),
+            width: MediaQuery.of(context).size.width - 30.0,
+            height: MediaQuery.of(context).size.height - 190.0,
+            child: ListView.builder(
+              itemCount: listMenu.length,
+              itemBuilder: (context, index) {
+                if (listMenu[index].category == category) {
+                  return _buildCard(listMenu[index], context);
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildCard(Menu menu, context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(5,5,5,10),
+      padding: const EdgeInsets.fromLTRB(5, 5, 5, 10),
       child: InkWell(
           onTap: () {},
           child: Container(
@@ -60,12 +61,9 @@ class MenuPage extends StatelessWidget {
                                 Icons.favorite,
                                 color: Color.fromARGB(255, 245, 89, 81),
                               )
-                            :  SizedBox(
-                                height: 25.0
-                              )
+                            : SizedBox(height: 25.0)
                       ],
-                    )
-                ),
+                    )),
                 Hero(
                   tag: menu.imageUrl,
                   child: Container(
@@ -98,20 +96,20 @@ class MenuPage extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(5,30,5,10),
+                  padding: const EdgeInsets.fromLTRB(5, 30, 5, 10),
                   child: Container(
                     color: Color.fromARGB(255, 231, 231, 231),
                     height: 0.8,
                   ),
                 ),
                 InkWell(
-                  onTap: () {
-                    print("Added to Cart"); 
-                  }, 
-                  child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                    onTap: () {
+                      print("Added to Cart");
+                    },
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                         child: Container(
-                          padding: EdgeInsets.all(5),
+                          padding: EdgeInsets.all(10),
                           decoration: BoxDecoration(
                             color: Color(0xffF55951),
                             borderRadius: BorderRadiusDirectional.circular(10),
@@ -135,9 +133,7 @@ class MenuPage extends StatelessWidget {
                               ),
                             ],
                           ),
-                        )
-                  )
-                )
+                        )))
               ],
             ),
           )),
