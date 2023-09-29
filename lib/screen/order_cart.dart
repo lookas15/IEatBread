a1221a022928920465fdb9b10256c4385d714e33
garcia <emmanoelgarsia@gmail.com>
2023-09-29 15:18:45 +0700

    details + order cart

--- lib/screen/order_cart.dart
+++ lib/screen/order_cart.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/data/DBHelper.dart';
+import 'package:flutter_application/data/cart_provider.dart';
+import 'package:flutter_application/models/cart_model.dart';
 import 'package:flutter_application/screen/menu_list.dart';
 import 'package:flutter_application/widget/navbar_widget.dart';
+import 'package:flutter_application/widget/plus_minus_buttons.dart';
+import 'package:flutter_application/widget/subtotal_widget.dart';
+import 'package:provider/provider.dart';
 
 class OrderCart extends StatefulWidget {
   const OrderCart({super.key});
@@ -9,12 +15,22 @@ class OrderCart extends StatefulWidget {
   State<OrderCart> createState() => _OrderCartState();
 }
 
-class _OrderCartState extends State<OrderCart> with SingleTickerProviderStateMixin {
+class _OrderCartState extends State<OrderCart>
+    with SingleTickerProviderStateMixin {
+  DBHelper? dbHelper = DBHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<CartProvider>().getData();
+  }
 
   int selectedBottomNavIndex = 3;
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context);
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 240, 240, 240),
       appBar: AppBar(
@@ -31,16 +47,228 @@ class _OrderCartState extends State<OrderCart> with SingleTickerProviderStateMix
         centerTitle: true,
         leading: IconButton(
           icon: const Icon(
-            Icons.arrow_back, // Ganti ikon ke Icons.arrow_back
+            Icons.arrow_back,
             color: Colors.white,
           ),
           onPressed: () {
-            // Kembali ke layar sebelumnya
             Navigator.of(context).pushReplacement(
               MaterialPageRoute(builder: (context) => MenuList()),
             );
           },
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer<CartProvider>(
+              builder: (BuildContext context, provider, widget) {
+                if (provider.cart.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      'Your Cart is Empty',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18.0),
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: provider.cart.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        color: Colors.white,
+                        elevation: 5.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Image(
+                                height: 120,
+                                width: 180,
+                                image: AssetImage(provider.cart[index].image!),
+                              ),
+                              SizedBox(
+                                // width: 200,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    const SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    RichText(
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      text: TextSpan(
+                                        text: 'Menu: ',
+                                        style: TextStyle(
+                                          color: Colors.blueGrey.shade800,
+                                          fontSize: 16.0,
+                                          fontFamily: 'Poppins',
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text:
+                                                '${provider.cart[index].productName!}\n',
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    RichText(
+                                      maxLines: 1,
+                                      text: TextSpan(
+                                        text: 'Unit: ',
+                                        style: TextStyle(
+                                          color: Colors.blueGrey.shade800,
+                                          fontSize: 16.0,
+                                          fontFamily: 'Poppins',
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text:
+                                                '${provider.cart[index].category!}\n',
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    RichText(
+                                      maxLines: 1,
+                                      text: TextSpan(
+                                        text: 'Price/each: ' r"IDR ",
+                                        style: TextStyle(
+                                          color: Colors.blueGrey.shade800,
+                                          fontSize: 16.0,
+                                          fontFamily: 'Poppins',
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text:
+                                                '${provider.cart[index].productPrice.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match match) => '${match[1]}.')}\n',
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              ValueListenableBuilder<int>(
+                                valueListenable: provider.cart[index].quantity!,
+                                builder: (context, val, child) {
+                                  return PlusMinusButtons(
+                                    addQuantity: () {
+                                      setState(() {
+                                        cart.addQuantity(
+                                            provider.cart[index].id!);
+                                        dbHelper!
+                                            .updateQuantity(Cart(
+                                                id: index,
+                                                productId: index.toString(),
+                                                productName: provider
+                                                    .cart[index].productName,
+                                                initialPrice: provider
+                                                    .cart[index].initialPrice,
+                                                productPrice: provider
+                                                    .cart[index].productPrice,
+                                                quantity: ValueNotifier(provider
+                                                    .cart[index]
+                                                    .quantity!
+                                                    .value),
+                                                category: provider
+                                                    .cart[index].category,
+                                                image:
+                                                    provider.cart[index].image))
+                                            .then((value) {
+                                          cart.addTotalPrice(double.parse(
+                                              provider.cart[index].productPrice
+                                                  .toString()));
+                                        });
+                                      });
+                                    },
+                                    deleteQuantity: () {
+                                      setState(() {
+                                        cart.deleteQuantity(
+                                            provider.cart[index].id!);
+                                        cart.removeTotalPrice(double.parse(
+                                            provider.cart[index].productPrice
+                                                .toString()));
+                                      });
+                                    },
+                                    text: val.toString(),
+                                  );
+                                },
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: InkWell(
+                                      onTap: () {
+                                        dbHelper!.deleteCartItem(
+                                            provider.cart[index].id!);
+                                        provider.removeItem(
+                                            provider.cart[index].id!);
+                                      },
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.red,
+                                          borderRadius:
+                                              BorderRadius.circular(5.0),
+                                        ),
+                                        padding: EdgeInsets.all(10.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.delete,
+                                              color: Colors.white,
+                                            ),
+                                            Text(
+                                              'Delete Menu',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+          Consumer<CartProvider>(
+            builder: (BuildContext context, value, Widget? child) {
+              final ValueNotifier<int?> totalPrice = ValueNotifier(null);
+              for (var element in value.cart) {
+                totalPrice.value =
+                    (element.productPrice! * element.quantity!.value) +
+                        (totalPrice.value ?? 0);
+              }
+              return SubTotalWidget(totalPrice: totalPrice);
+            },
+          )
+        ],
       ),
       bottomNavigationBar: NavbarWidget(
         selectedIndex: selectedBottomNavIndex,
