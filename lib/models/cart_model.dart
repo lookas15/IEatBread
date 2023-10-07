7d732c97b8ed3542ec3dc3e1043a4351fd5408bc
XTu17As <richard91.us@gmail.com>
2023-10-08 00:17:38 +0700

    ORDER HISTORY UPDATE

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -55,12 +55,12 @@ class Order {
   final String id;
   final double amount;
   final List<Cart> products;
-  final DateTime dateTime;
+  final String dateTime; 
 
   Order({
     required this.id,
     required this.amount,
     required this.products,
-    required this.dateTime,
+    required this.dateTime, 
   });
 }
