3cb393b3fd5d141dc2e302b7598933679badac61
vinnatz <bezelx1@gmail.com>
2023-10-07 15:26:35 +0700

    Reformatting Text value

--- lib/widgets/subtotal_widget.dart
+++ lib/widgets/subtotal_widget.dart
@@ -18,7 +18,8 @@ class SubTotalWidget extends StatelessWidget {
           builder: (context, val, child) {
             return ReusableWidget(
               title: 'Sub-Total',
-              value: r'IDR ' + (val?.toStringAsFixed(2) ?? '0'),
+              value:
+                  'IDR ${val?.toStringAsFixed(2).replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match match) => '${match[1]}.') ?? '0'}',
             );
           },
         ),
